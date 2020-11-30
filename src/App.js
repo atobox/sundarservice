8458e929c874dde9beccceb58cb5c09979ebfa57
Eagle <anishmajumdar.ato@gmail.com>
2020-11-30 17:36:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './Images/logo_a.png';
+import About from './Components/about';
+import Book from './Components/book';
+import Store from './Components/store';
+import Team from './Components/team';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navbar">
+        <div><img src={logo} alt="logo" /></div>
+        <div><h1>SundarService<span className="in">.in</span></h1></div>
+        <div>
+          <ul>
+            <li><a href="#about"><i className="fas fa-info"></i> About Us</a></li>
+            <li><a href="#store"><i className="fas fa-store-alt"></i> Store</a></li>
+            <li><a href="#team"><i className="fas fa-user-friends"></i> Team</a></li>            
+            <li><a href="#book">Book Call <i className="fas fa-bell"></i></a></li>
+          </ul>
+        </div>        
+      </div>     
+      <About />
     </div>
   );
 }
---
+++ src/Components/about.js
@@ -0,0 +1,14 @@
+import thinking from '../Images/thinking.svg';
+
+export default function About() {
+
+    return (
+        <div>
+            <div className="cta_txt">Leave your I.T. headache on us.</div>            
+            <div className="cta_btn"><div>Consult Us</div></div>
+            <div className="svg">
+                <img src={thinking} alt="svg" />
+            </div>
+        </div>
+    );
+}
