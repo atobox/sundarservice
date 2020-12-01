89755fc471c700fb11428b20b8fa9c80cd750ffe
Eagle <anishmajumdar.ato@gmail.com>
2020-12-01 13:21:50 +0530

    css chk1

--- src/Components/book.js
+++ src/Components/book.js
@@ -3,10 +3,12 @@ import thinking from '../Images/thinking.svg';
 export default function About() {    
     return (
         <div id="book">
-            <div className="cta_txt">Leave your I.T. headache on us.</div>            
-            <div className="cta_btn"><div>Consult Us</div></div>
-            <div className="svg">
-                <img src={thinking} alt="svg"/>
+            <div style={{maxWidth:"1100px", margin:"0 auto"}}>
+                <div className="cta_txt">Leave your I.T. headache on us.</div>            
+                <div className="cta_btn"><div>Consult Us</div></div>
+                <div className="svg">
+                    <img src={thinking} alt="svg"/>
+                </div>
             </div>
         </div>
     );
