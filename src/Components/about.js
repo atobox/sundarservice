f6d0047e3be7abe8f55fed7150b70e9a41be2a88
Eagle <anishmajumdar.ato@gmail.com>
2020-12-01 12:54:21 +0530

    css correction

--- src/Components/about.js
+++ src/Components/about.js
@@ -4,12 +4,14 @@ export default function About() {
 
     return (
         <div className="abt_sec" id="about">
-            <img src={abt_img} alt="abt_img" />
-            <div className="abt_art">
-                <span className="abt_qstn">Who we are and why should you <span className="in">care</span>?</span><br/>
-                <span className="abt_ans">
-                It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
-                </span>
+            <div style={{width:"1000px", margin:"0 auto", height: "320px"}}>
+                <img src={abt_img} alt="abt_img" />
+                <div className="abt_art">                
+                    <span className="abt_qstn">Who we are and why should you <span className="in">care</span>?</span><br/>
+                    <span className="abt_ans">
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+                    </span>
+                </div>
             </div>
         </div>
     );
