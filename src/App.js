22380afe3cb595976cfd7a29c35e29738972dc70
Eagle <anishmajumdar.ato@gmail.com>
2020-12-01 22:40:00 +0530

    desktop css complete

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import About from './Components/about';
 import Book from './Components/book';
 import Store from './Components/store';
 import Team from './Components/team';
+import GoogleMapReact from 'google-map-react';
 
 function App() {
+  let vl = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   return (
     <div className="App">
       <div className="navbar">
@@ -23,6 +32,41 @@ function App() {
       <Book />
       <About />
       <Store />
+      <Team />
+      <footer>
+        <h3>SundarService<span className="in">.in</span></h3>
+        <ul>
+          <h3>Browse Links</h3>
+          <li><a href="#book">Contact Us</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#store">Our Store</a></li>
+          <li><a href="#team">Our Team</a></li>                    
+        </ul>
+        <div className="map">
+        <GoogleMapReact        
+        defaultCenter={vl.center}
+        defaultZoom={vl.zoom}
+        >
+      </GoogleMapReact>
+        </div>        
+      </footer>
+      <div className="sup_footer">
+          <p>SundarService<span className="in" style={{background:"black"}}>.in</span> &copy; 2020</p>
+          <div style={{
+            width:"300px",
+            lineHeight:"25px",
+            position:"absolute",
+            top:"5px",
+            left: "300px",
+            fontSize: "14px",
+            fontWeight: "bold",
+            letterSpacing: "1px",
+          }}>
+            <i className="fa fa-phone"></i> (+91) 76793 78165<br/>
+            <i className="fa fa-envelope"></i> sundarservice@gmail.com
+          </div>
+          <span className="dcredit">Designed & developed by Anish Majumdar</span>
+      </div>
     </div>
   );
 }
--- src/Components/team.js
+++ src/Components/team.js
@@ -1,6 +1,35 @@
+import el from '../Images/team/el.jpg';
+import bill from '../Images/team/bill.jpg';
+import steve from '../Images/team/steve.jpg';
+
+function PersonCard(props)  {
+
+    return (
+        <div className="prsncrd">
+            <div className="img_brdr">
+                <img src={props.src} alt="person_image" />
+            </div>
+            <p className="name">{props.name}</p>
+            <div className={props.isMain ? "txt_brdr_main" : "txt_brdr"}>
+                <p className="task">{props.task}</p>
+                <i className="fa fa-facebook-square"></i>
+                <i className="fa fa-instagram"></i>
+                <i className="fa fa-twitter-square"></i>
+            </div>
+        </div>
+    );
+}
+
 export default function Team() {
 
     return (
-        <div>Our Team...</div>
+        <div id="team" className="team_cont">
+            <h2>Meet our <span className="in">Team</span></h2>
+            <div className="team_content">  
+                <PersonCard src={el} name="Elon Musk" task="CEO & Visionary"/>
+                <PersonCard src={bill} name="Bill Gates" task="Director & Philanthropist" />
+                <PersonCard src={steve} name="Steve Jobs" task="Director & Creative Genius" isMain="true"/>
+            </div>
+        </div>
     );
 }
